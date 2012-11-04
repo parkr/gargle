1f410350ee6ec7972e6e7d8b7e5b9176b77eead2
Parker Moore <parkrmoore@gmail.com>
2012-11-04 01:03:34 -0400

    Basics of the PageRankr

---
+++ page_rankr.py
@@ -0,0 +1,13 @@
+class PageRankr:
+    def __init__(self, d=0.85, size=(0,0)):
+        self.__d = d
+        self.__m = numpy.zeros(size)
+        self.__pages = []
+    
+    def add_page(self, page):
+        self.__pages.append(page)
+    
+    def calc_page_ranks(self):
+        raise RuntimeError('PageRankr#calc_page_ranks has not been implemented yet.')
+        # re-create self.__m
+        
