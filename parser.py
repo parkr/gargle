566b22ead9570de03de4dec6dedecd3321d4f701
Parker Moore <parkrmoore@gmail.com>
2012-11-04 21:36:52 -0500

    Calculating normalized page ranks

--- parser.py
+++ parser.py
@@ -3,36 +3,17 @@ from xml.dom.minidom import Document
 from datetime import datetime
 from page import Page
 from progressbar import *
-import urllib2, time, sys
+import urllib2, helpers, numpy
 
 class Parser:
     
     def __init__(self):
+        self.S = []
         self.urls = []
         self.pages = []
         self.pages_with_ids = {}
-        self.backup_pages_dir_rel_path = "./test3"
-        
-    def __get_html(self, num, url):
-        encoding = False
-        try:
-            f = open("%s/%s.html" % (self.backup_pages_dir_rel_path, num), 'r')
-        except IOError:
-            try:
-                f = open("%s/%s.htm" % (self.backup_pages_dir_rel_path, num), 'r')
-                encoding = "iso-8859-1"
-            except IOError:
-                return None
-        
-        html = f.read()
-        try:
-            unicode_html = unicode(html, 'utf-8')
-        except UnicodeDecodeError:
-            if encoding:
-                unicode_html = unicode(html, encoding)
-            else:
-                unicode_html = unicode(html, 'windows-1252')
-        return unicode_html
+        self.urls_with_nums = {}
+        self.ranks = None
     
     def parse_urls(self, filename='./test/test3.txt'):
         # reads in URLs & normalizes
@@ -40,28 +21,76 @@ class Parser:
         for line in f.readlines():
             els = line.strip().split(',')
             self.urls.append( (els[0], els[1]) )
+            self.S.append(els[1])
     
     def process_pages(self):
         skipped = []
         pbar = ProgressBar(widgets=['Processing pages: ', SimpleProgress()], maxval=len(self.urls)).start()
+        i = 0
         
         for (num, url) in self.urls:
             pbar.update(int(num))
             if (num and url):
-                html = self.__get_html(num, url)
+                html = helpers.get_html(num, url)
                 if html is not None:
+                    self.urls_with_nums[url] = num
                     soup = BeautifulSoup(html.encode('utf-8', 'ignore'), 'lxml')
-                    page = Page(title=soup.title.string, num=num, html=soup.prettify())
-                    self.pages_with_ids[page.ID] = page
+                    page = Page(title=soup.title.string, num=num, html=soup.prettify(), url=url)
+                    page.index = i
+                    if page.ID not in self.pages_with_ids.keys():
+                        self.pages_with_ids[page.ID] = page
+                    else:
+                        raise RuntimeError('COLLISION: %s collides with %s with hash %s.' % (page.num, self.pages_with_ids[page.ID].num, page.ID))
                     for link in soup.find_all('a'):
-                        page.a.append(link)
+                        if link.get('href') and 'mailto:' != link.get('href').strip()[0:7]:
+                            page.a.append(link)
                     self.pages.append(page)
+                    i += 1
                 else:
                     skipped.append(num)
             else:
                 skipped.append(num)
         pbar.finish()
         print "Skipped page(s) %s because of an error." % (', '.join(skipped))
+    
+    def calc_page_ranks(self, d=0.85):
+        # re-create self.__m
+        self.ranks = numpy.zeros( (len(self.pages_with_ids),len(self.pages_with_ids)) )
+        pbar = ProgressBar(widgets=['Processing links: ', SimpleProgress()], maxval=len(self.pages_with_ids.keys())).start()
+        progress = 1
+        
+        for (ID, page) in self.pages_with_ids.iteritems():
+            pbar.update(progress)
+            # magic PageRank
+            for a in page.a:
+                href = a.get('href')
+                # normalize URLS
+                url = page.normalize_url(href)
+                if url in self.S:
+                    soup = BeautifulSoup(helpers.get_html(self.urls_with_nums[url]).encode('utf-8', 'ignore'), 'lxml')
+                    ID = helpers.page_hash(soup.prettify())
+                    if ID in self.pages_with_ids.keys():
+                        #print "%s (#%d) cites %s (#%d)" % (page.num, page.index, self.pages_with_ids[ID].num, self.pages_with_ids[ID].index)
+                        #print self.urls[int(self.pages_with_ids[ID].num)-1]
+                        self.ranks[page.index][self.pages_with_ids[ID].index] = 1.0
+                #print a.get_text()
+            progress += 1
+        # Normalize adjacency matrix into PageRanks
+        pbar = ProgressBar(widgets=['Normalizing PageRanks: ', SimpleProgress()], maxval=len(self.pages_with_ids.keys())).start()
+        progress = 1
+        col_sums = numpy.sum(self.ranks, axis=1)
+        print col_sums
+        for (ID, page) in self.pages_with_ids.iteritems():
+            pbar.update(progress)
+            for k in xrange(len(self.ranks[page.index])):
+                if col_sums[k] > 0:
+                    self.ranks[page.index][k] = self.ranks[page.index][k] / col_sums[page.index]
+                else:
+                    self.ranks[page.index][k] = 0.0
+            progress += 1
+            
+        pbar.finish()
+        numpy.savetxt("page_ranks.txt", self.ranks)
     
     def write_metadata(self):
         # write metadata.xml
