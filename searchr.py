281775cd89d5f7b308f2a88df5c37d3b051fe8a9
Parker Moore <parkrmoore@gmail.com>
2012-11-04 22:49:42 -0500

    Wrote logic for building index and printing query output

--- searchr.py
+++ searchr.py
@@ -1,4 +1,36 @@
+from progressbar import ProgressBar, SimpleProgress
+from operator import itemgetter
+from brain import Brain
+
 class Searchr:
     
+    def __init__(self, brain=Brain()):
+        self.brain = brain
+        self.anchor_text_with_ranks_with_pages = {}
+    
+    # Public: Create index based on the ranks in the brain
+    def build_index(self):
+        pbar = ProgressBar(widgets=['Processing pages: ', SimpleProgress()], maxval=len(self.brain.pages_with_ids.keys())).start()
+        index = 1
+        for (ID, page) in self.brain.pages_with_ids.iteritems():
+            pbar.update(index)
+            for a in page.a:
+                anchor_text = a.get_text()
+                if anchor_text in self.anchor_text_with_ranks_with_pages.keys():
+                    self.anchor_text_with_ranks_with_pages[anchor_text].append((page.rank, page))
+                else:
+                    self.anchor_text_with_ranks_with_pages[anchor_text] = [(page.rank, page)]
+            index += 1
+        pbar.finish()
+    
     def process_query(self, query):
-        pass
+        results = []
+        already_collected = []
+        for anchor_text in self.anchor_text_with_ranks_with_pages.keys():
+            if query in anchor_text:
+                for pages in self.anchor_text_with_ranks_with_pages[anchor_text]:
+                    if pages[1].ID not in already_collected:
+                        results.append(pages)
+                        already_collected.append(pages[1].ID)
+        for (rank, page) in sorted(results, key=itemgetter(0)):
+            print page.search_output()
